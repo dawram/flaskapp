5649e6ab59e4513edb7252d6d285f5ec637f91b8
Dawid <dawid.ramokepa@gmail.com>
2018-04-11 18:45:07 +0200

    pas change

--- dummy.py
+++ dummy.py
@@ -12,7 +12,7 @@ session = Session()
 user = User("admin", "password")
 session.add(user)
 
-user = User("python", "python")
+user = User("python", "pythons")
 session.add(user)
 
 user = User("jumpiness", "python")
